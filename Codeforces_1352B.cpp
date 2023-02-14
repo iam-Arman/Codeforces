1dcf4f4731d4da706d676cbf012d7dd719ab14fe
Shahriar Mahmud Arman <iamarman252575@gmail.com>
2023-02-14 18:32:57 +0600

    Create Codeforces_1352B.cpp

---
+++ Codeforces_1352B.cpp
@@ -0,0 +1,206 @@
+                                          //   Bismillahir Rahmanir Rahim      //
+                                         //      SHAHRIAR MAHMUD ARMAN        //
+                                        //         AUTHOR : iamarman         //
+
+
+#include<bits/stdc++.h>
+
+                                             ////       MACROS       ////
+
+//---------------------------------------------------------------------------------------------------------------------------------|
+
+# define ll long long                                                                                                              
+# define test int test; cin>>test; while(test--)
+# define rep(i,a,b) for(int i=a;i<b;i++)
+# define rep_n(i,a,b) for(int i=a;i<=b;i++)
+# define rep_rev(i,a,b) for(int i=b;a>=1;i--)
+# define pb push_back
+# define el "\n"
+# define optimise   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); cin.clear(); 
+# define R0 return 0
+# define all(v) v.begin(),v.end()
+# define srt(vvv) sort(vvv.begin(),vvv.end())
+# define sp " "
+# define vii vector<int>
+# define vll vector<long long>
+# define yess cout<<"YES"<<el
+# define noo cout<<"NO"<<el
+# define PI 2*acos(0.0)
+ll mod=10000000+7;
+
+//----------------------------------------------------------------------------------------------------------------------------------|
+
+
+//                                             ///      NAMESPACE     ///
+
+using namespace std;
+
+
+                     /// THIS IS FOR GIVING INPUT AND GETTING OUTPUT IN A FILE INSTEAD OF TERMINAL OR CONSOLE   ///
+
+
+inline void file() {
+
+#ifndef ONLINE_JUDGE
+
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+}
+
+
+                                                    /////    FUNCTIONS     /////
+
+
+
+ll bigmod(ll base,ll power)
+{
+  ll res=1;
+  ll p=base%mod;
+  while(power>0)
+  {
+    if(power%2==1)
+    {
+      res=((res%mod)*(p%mod))%mod;
+      //res*=p;
+    }
+    power/=2;
+    p=(p%mod)*(p%mod);
+  }
+  return res;
+}
+
+
+long long LOG(long long base, long long n)
+{  
+  long long cnt=0;
+while(n>0)
+{
+ n=n/base;
+ cnt++;
+}
+return (cnt-1);
+}
+
+
+ll poww(ll a,ll b)
+{
+  ll ans=1;
+  if(!b) return 1;
+  while(b>1)
+  {
+    if(b&1)
+    {
+      ans=ans*a%mod;
+    }
+    a=a*a%mod;
+    b/=2;
+  }
+  return a*ans%mod;
+}
+
+int gcd(int a,int b)
+{
+  int rem;
+  while(b%a!=0)
+  {
+    rem=b%a;
+    b=a;
+    a=rem;
+  }
+  return a;
+}
+
+
+int GCD(ll a,ll b)
+{
+  if(a==b) return a;
+  else if(b==0) return a;
+  else return GCD(b,a%b);
+}
+
+
+
+
+                                                    ///  CODE STARTS FROM HERE    ///
+
+
+
+
+void solve()
+{
+    
+    int n,k;
+    cin>>n>>k;
+
+    if(k>n)
+    {
+      cout<<"NO"<<endl;
+      return;
+    }
+
+    if(n==k)
+    {
+      cout<<"YES"<<endl;
+      for(int i=0;i<n;i++)
+      {
+        cout<<1<<sp;
+      }
+      cout<<endl;
+      return;
+    }
+    if(n%2==0 and n>=2*k)
+    {
+      cout<<"YES"<<endl;
+      for(int i=0;i<k-1;i++)
+      {
+        cout<<2<<sp;
+        n-=2;
+      }
+      cout<<n<<endl;
+      return;
+    }
+    if(n%2==0 and n<2*k and k%2==0)
+    {
+      cout<<"YES"<<endl;
+      for(int i=0;i<k-1;i++)
+      {
+        cout<<1<<sp;
+        n-=1;
+      }
+      cout<<n<<endl;
+      return;
+    }
+
+    if(n%2==1 and k%2==1)
+    {
+      cout<<"YES"<<endl;
+      for(int i=0;i<k-1;i++)
+      {
+        cout<<1<<sp;
+        n-=1;
+      }
+      cout<<n<<endl;
+      return;
+    }
+    cout<<"NO"<<endl;
+      
+
+
+}
+
+int main()
+{
+    optimise;
+    file();
+    test{
+
+    solve();
+
+    }
+
+}
+
+
+// Problem Link : https://codeforces.com/contest/1352/problem/B
